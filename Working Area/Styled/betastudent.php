79af59edc8c01ab721a6986d5f4e810a969e1ec8
Emil Karbanov <random1hole@gmail.com>
2015-06-28 11:51:21 +0300

    Update betastudent.php

--- Working Area/Styled/betastudent.php
+++ Working Area/Styled/betastudent.php
@@ -17,11 +17,11 @@
 		border-style:solid;
 		border-width: 3px;
 		width:85%;
-		margin-top: 5%;
+		margin-top: 3%;
 		margin-bottom:4%;
 		margin-left:auto;
 		margin-right:auto;
-		padding:4%;
+		padding:3%;
 	}
 	body {
 		background-size : 100%;
